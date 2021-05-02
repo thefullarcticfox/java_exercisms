5495e0d888bd8dbf259dd595c6321a64f01d6e40
Ivan <thefullarcticfox@users.noreply.github.com>
2021-05-02 20:55:47 +0300

    +1 exercise

---
+++ two-fer/src/main/java/Twofer.java
@@ -0,0 +1,7 @@
+public class Twofer {
+    public String twofer(String name) {
+        if (name == null)
+            name = "you";
+        return String.format("One for %s, one for me.", name);
+    }
+}
